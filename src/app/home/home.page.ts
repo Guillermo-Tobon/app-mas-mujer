9485ea80fa72e781527417824aed8f235d1c8086
Guillermo-Tobon <memot18@hotmail.com>
2020-09-16 21:03:18 -0500

    Validación del formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public exprNum:string = "/^[0-9]$/";
+  public formRegistro: FormGroup;
+
+  constructor(
+              private formBuilder: FormBuilder
+  ) {
+    this.formRegistro = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      numeroIdentifica: ['', [Validators.required, Validators.minLength(6)]],
+      direccion: ['', Validators.required],
+      ciudad: ['',Validators.required],
+      departamento: ['',Validators.required],
+      telefono: ['', [Validators.required, Validators.minLength(10)]],
+      correo: ['', [Validators.required, Validators.email]],
+      nombreContacto1: ['', Validators.required],
+      telefonoContacto1: ['', [Validators.required, Validators.minLength(10)]],
+      nombreContacto2: ['', Validators.required],
+      telefonoContacto2: ['', [Validators.required, Validators.minLength(10)]],
+      terminos: ['',Validators.required]
+    })
+  }
+
+
+  /**
+   * Método para guardar la data del formulario de registro
+   */
+  public GuardarDataForm(){
+    console.log(this.formRegistro.value);
+  }
 
 }
