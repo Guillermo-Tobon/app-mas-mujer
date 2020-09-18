ddfc7db29f9f060f6fa83a1bdc3c4deac0bffe45
Guillermo-Tobon <memot18@hotmail.com>
2020-09-18 12:17:34 -0500

    Validación con checkbox

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   public exprNum:string = "/^[0-9]$/";
   public formRegistro: FormGroup;
 
   constructor(
-              private formBuilder: FormBuilder
+              private formBuilder: FormBuilder,
+              private alertCtrl: AlertController
   ) {
     this.formRegistro = this.formBuilder.group({
       nombre: ['', Validators.required],
@@ -26,8 +28,12 @@ export class HomePage {
       telefonoContacto1: ['', [Validators.required, Validators.minLength(10)]],
       nombreContacto2: ['', Validators.required],
       telefonoContacto2: ['', [Validators.required, Validators.minLength(10)]],
-      terminos: ['',Validators.required]
+      terminos: ['false',Validators.required]
     })
+  }
+
+  ngOnInit(){
+    
   }
 
 
@@ -35,7 +41,44 @@ export class HomePage {
    * Método para guardar la data del formulario de registro
    */
   public GuardarDataForm(){
-    console.log(this.formRegistro.value);
+    if (this.formRegistro.value.terminos === "false") {
+      this.alertTerminos();
+
+    } else {
+      console.log(this.formRegistro.value);
+
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
+  /********************************************
+   * Métodos de validaciones 
+   ********************************************/
+  /**
+   * Método alert async para validar los datos del form
+   */
+  async alertTerminos(){
+    const alert = await this.alertCtrl.create({
+      header: 'Advertencia',
+      subHeader: 'Falta términos y condiciones',
+      message: 'Has diligenciado bien el formulario, pero falta aceptar términos y condiciones.\n Si tienes dudas o inquietudes, puedes leer los términos en el botón de "Leer más."',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+
+
+
+
 
 }
